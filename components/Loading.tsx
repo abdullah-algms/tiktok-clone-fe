2450d7a5ec18292b8f5d145db7dc2373aded92b7
mnuralim <shadownur345.com>
2023-09-26 10:43:55 +0800

    update

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const LoadingBar = () => {
-  return <div className="w-full h-[2px] bg-gradient-to-r from-[#FECD00] via-[#F9373F] to-[#C913B9] animate-slide-right absolute top-1 duration-700"></div>;
+  return <div className="w-full h-[2px] bg-gradient-to-r from-[#FECD00] via-[#F9373F] to-[#C913B9] animate-slide-right absolute top-[6px] duration-700"></div>;
 };
